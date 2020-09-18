555cc1529240f150b1e3ce89fa9c1d5ab60a0ea5
amdiaznyc <amdiaznyc@gmail.com>
2020-09-17 21:10:45 -0400

    3.9 Validating Data with 'set' Functions

--- GradeBook.h
+++ GradeBook.h
@@ -1,15 +1,14 @@
-// Fig. 3.11: GradeBook.h
-// GradeBook class definition. This file presents GradeBook's public
-// interface without revealing the implementation of GradeBook's member
-// functions, which are defined in GradeBook.cpp.
-#include <string> // class GradeBook uses C++ standard string class
+// Fig. 3.15: GradeBook.h
+// GradeBook class definition presents the public interface of
+// the class. Member-function definitions appear in GradeBook.cpp.
+#include <string> // program uses C++ standard string class
 using namespace std;
 
 // GradeBook class definition
 class GradeBook
 {
 public:
-    GradeBook( string ); // constructor that initializes courseName
+    GradeBook( string ); // constructor that initializes a GradeBook object
     void setCourseName( string ); // function that sets the course name
     string getCourseName(); // function that gets the course name
     void displayMessage(); // function that displays a welcome message
