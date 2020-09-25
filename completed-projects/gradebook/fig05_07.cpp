a90d9560ef83edf584381bba0ef49980851ea789
amdiaznyc <amdiaznyc@gmail.com>
2020-09-25 14:13:43 -0400

    5.5 'do...while' Repetition Statement

---
+++ completed-projects/gradebook/fig05_07.cpp
@@ -0,0 +1,17 @@
+// Fig. 5.7: fig05_07.cpp
+// 'do...while' repetition statement.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int counter = 1; // initalize counter
+
+    do
+    {
+        cout << counter << " "; // display counter
+        counter++; // increment counter
+    } while ( counter <= 10 ); // end do...while
+
+    cout << endl; // output a newline
+} // end main
