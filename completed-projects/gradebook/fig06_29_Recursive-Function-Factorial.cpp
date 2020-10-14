b637b5423027870076063b4fa95b78b69b81c8ef
amdiaznyc <amdiaznyc@gmail.com>
2020-10-14 09:46:30 -0400

    6.19 Recursion

---
+++ completed-projects/gradebook/fig06_29_Recursive-Function-Factorial.cpp
@@ -0,0 +1,24 @@
+// Fig. 6.29: fig06_29.cpp
+// Demonstrating the recursive function factorial.
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+unsigned long factorial( unsigned long ); // function prototype
+
+int main()
+{
+    // calculate the factorials of 0 through 10
+    for ( int counter = 0; counter <= 10; counter++ )
+        cout << setw( 2 ) << counter << "! = " << factorial( counter )
+            << endl;
+} // end main
+
+// recursive definition of function factorial
+unsigned long factorial( unsigned long number )
+{
+    if ( number <= 1 ) // test for base case
+        return 1; // base cases: 0! = 1 and 1! = 1
+    else // recursion step
+        return number * factorial( number - 1 );
+} // end function factorial
