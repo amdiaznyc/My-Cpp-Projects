601eab74a68a734ce664e15ba531bee86cbf999f
amdiaznyc <amdiaznyc@gmail.com>
2020-10-14 15:44:15 -0400

    7.5 Passing Arrays to Functions

---
+++ completed-projects/gradebook/fig07_14-Demonstrating-the-Const-Type_Qualifier.cpp
@@ -0,0 +1,23 @@
+// Fig. 7.14: fig07_14.cpp
+// Demonstrating the const type qualifier.
+#include <iostream>
+using namespace std;
+
+void tryToModifyArray( const int [] ); // function prototype
+
+int main()
+{
+    int a[] = { 10, 20, 30 };
+
+    tryToModifyArray( a );
+    cout << a[ 0 ] << ' ' << a[ 1 ] << ' ' << a[ 2 ] << '\n';
+} // end main
+
+// In function tryToModifyArray, "b" cannot be used
+// to modify the original array "a" in main.
+void tryToModifyArray( const int b[] )
+{
+    b[ 0 ] /= 2; // compilation error
+    b[ 1 ] /= 2; // compilation error
+    b[ 2 ] /= 2; // compilation error
+} // end function tryToModifyArray
